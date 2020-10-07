c619396dcf26f919b1a82562cce744a41a6f1305
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-10-07 20:38:46 +1100

    Create object

--- src/DataStructures/SemanticIdentity.js
+++ src/DataStructures/SemanticIdentity.js
@@ -2,14 +2,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+ import { createUUID } from "./SemanticDomain";
+
 export class SemanticIdentity {
 
-    constructor(UUID, name, description, abbreviation, shortAbbreviation, translations){
-        this.UUID = UUID;
+    constructor(name, description, abbreviation, shortAbbreviation, UUID, translations){
+        if (UUID !== null){
+            this.UUID = UUID;
+        } else {
+            this.UUID = createUUID();
+        }
+
         this.name = name;
         this.description = description;
         this.abbreviation = abbreviation;
         this.shortAbbreviation = shortAbbreviation;
-        this.translations = translations;
+
+        if (translations !== null){
+            this.translations = translations;
+        } else {
+            this.translations = [];
+        }
     }
 }
